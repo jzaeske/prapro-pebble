d3deca92d3497a9b5ca1404d14c73f24b898ab73
Jan Zaeske <mail@jzaeske.de>
2015-04-26 13:08:20 +0200

    Click Handling and Bugfix

--- src/trains.c
+++ src/trains.c
@@ -3,6 +3,9 @@
 #define CHANNEL 1
 #define SPEED 2
 
+#define MODE_CHANNEL 3;
+#define MODE_SPEED 4;
+
 // UI Elements
 static Window *s_main_window;
 
@@ -20,22 +23,24 @@ static uint8_t s_sync_buffer[32];
 static char s_channel[] = "1";
 static char s_speed[] = "1";
 
+static uint8_t mode = MODE_SPEED;
+
 /*
  * Recieving messages from Android app
  */
 static void sync_changed_handler(const uint32_t key, const Tuple *new_tuple, const Tuple *old_tuple, void *context) {
   static char buffer[64];
+
   // da kommen komisch Werte an. nicht beachten
-  if (new_tuple->value->int32 > 7) return;
-  snprintf(buffer, sizeof(buffer), "%u -> %u", (uint)key, (uint)new_tuple->value->int32);
-  APP_LOG(APP_LOG_LEVEL_ERROR, buffer);
+  //snprintf(buffer, sizeof(buffer), "%u -> %u", (uint)key, (uint)new_tuple->value->uint8);
+  //APP_LOG(APP_LOG_LEVEL_ERROR, buffer);
   if (key == CHANNEL) {
     // channel
-    snprintf(s_channel, sizeof(s_channel), "%u", (int)new_tuple->value->int32);
+    snprintf(s_channel, sizeof(s_channel), "%u", (int)new_tuple->value->uint8);
     text_layer_set_text(s_channel_layer, s_channel);
   } else if (key == SPEED) {
     //speed
-    snprintf(s_speed, sizeof(s_speed), "%u", (int)new_tuple->value->int32);
+    snprintf(s_speed, sizeof(s_speed), "%d", (int)new_tuple->value->int8);
     text_layer_set_text(s_speed_layer, s_speed);
   }
 }
@@ -43,6 +48,34 @@ static void sync_changed_handler(const uint32_t key, const Tuple *new_tuple, con
 static void sync_error_handler(DictionaryResult dict_error, AppMessageResult app_message_error, void *context) {
   // An error occured!
   APP_LOG(APP_LOG_LEVEL_ERROR, "sync error!");
+}
+
+/*
+ * Click handler
+ */
+
+static void send_int(int key, int value) {
+  DictionaryIterator *iter;
+  app_message_outbox_begin(&iter);
+  dict_write_int(iter, key, &value, sizeof(int), true);
+  app_message_outbox_send();
+}
+
+static void down_single_click_handler(ClickRecognizerRef recognizer, void *context) {
+  send_int(mode, -1);
+}
+
+static void up_single_click_handler(ClickRecognizerRef recognizer, void *context) {
+  send_int(mode, 1);
+}
+
+void config_provider(Window *window) {
+ // single click / repeat-on-hold config:
+  window_single_click_subscribe(BUTTON_ID_DOWN, down_single_click_handler);
+  window_single_click_subscribe(BUTTON_ID_UP, up_single_click_handler);
+
+  // long click config:
+  //window_long_click_subscribe(BUTTON_ID_SELECT, 700, select_long_click_handler, select_long_click_release_handler);
 }
 
 static void update_layer(Layer *layer, GContext *ctx) {
@@ -99,6 +132,8 @@ static void init() {
 
   window_stack_push(s_main_window, true);
 
+  window_set_click_config_provider(s_main_window, (ClickConfigProvider) config_provider);
+
   // Setup AppSync
   app_message_open(app_message_inbox_size_maximum(), app_message_outbox_size_maximum());
 
@@ -109,7 +144,7 @@ static void init() {
   };
 
   // Begin using AppSync
-  app_sync_init(&s_sync, s_sync_buffer, sizeof(s_sync_buffer), initial_values, ARRAY_LENGTH(initial_values), sync_changed_handler, sync_error_handler, NULL);
+  app_sync_init(&s_sync, s_sync_buffer, sizeof(s_sync_buffer), initial_values, ARRAY_LENGTH(initial_values), sync_changed_handler, sync_error_handler, (void*) 4242);
 }
 
 static void deinit() {
